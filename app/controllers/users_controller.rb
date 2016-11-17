97729e47b29d8d220f6f38bccd99e073da01a4c2
Ekaterina Petushova <epetushova@gmail.com>
2016-11-17 17:44:38 -0500

    build users_controller.rb

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,51 @@
+class UsersController < ApplicationController
+
+		get '/signup' do
+			if is_logged_in?
+
+				redirect '/trips'
+			else
+				erb :'/users/signup'
+			end
+		end
+		
+		get '/users/:id' do
+			@user = User.find_by_id(params[:id])
+			@tasks = @user.tasks
+			erb :'/users/show'
+		end
+		
+		post '/signup' do
+			@user = User.new(username: params[:username], email: params[:email], password: params[:password])
+			if @user.save
+				session[:user_id] = @user.id
+				redirect '/tasks'
+			else
+				erb :'/users/signup', locals: {message: "There seems to be an error. Please try again."}
+			end
+		end
+
+		get '/login' do
+			if !is_logged_in?
+				erb :'/users/login'
+			else
+				redirect '/tasks'
+			end
+		end
+
+		post '/login' do
+			user = User.find_by(username: params[:username])
+			if user && user.authenticate(params[:password])
+				session[:user_id] = user.id
+				redirect '/tasks'
+			else
+				erb :'/users/login', locals: {message: "There seems to be an error. Please try again."}
+			end
+		end
+		
+		get '/logout' do
+			session.clear
+			erb :'/index'
+			end
+		end
+end
