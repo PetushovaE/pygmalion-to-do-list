7e5dfe644d1984b14ef42dfdcbec19f00d1d0085
Ekaterina Petushova <epetushova@gmail.com>
2016-11-23 11:13:39 -0500

    added bootstrap to layout file

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,11 @@ class User < ActiveRecord::Base
 
 	has_secure_password
 
+	# def password=(pw)
+	# 	#  take pw and encrypt it
+
+	# 	# put encrypted pw into our database in the password_digest column
+	# end
+
 	validates_presence_of :username, :email, :password 
 end
